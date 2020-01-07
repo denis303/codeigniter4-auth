129b0979c579d774b70e0e303d8bde6b2f6952e8
denis303 <dev@denis303.com>
2020-01-08 01:40:37 +0300

    n/a

---
+++ AuthSession.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author denis303 <dev@denis303.com>
+ * @license MIT
+ * @link http://denis303.com
+ */
+namespace Denis303\Auth;
+
+use Config\Services;
+
+class AuthSession
+{
+
+    public $name;
+
+    protected $_session;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+
+        $this->_session = Services::session();
+    }
+
+    public function getAuthId()
+    {
+        return $this->_session->get($this->name);
+    }
+
+    public function setAuthId($auth)
+    {
+        $this->_session->set($this->name, $auth);
+    }
+
+    public function deleteAuthId()
+    {
+        $this->_session->remove($this->name);
+    }
+
+}
---
+++ UserService.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @author denis303 <dev@denis303.com>
+ * @license MIT
+ * @link http://denis303.com
+ */
+namespace Denis303\Auth;
+
+use Denis303\NotRememberMe\NotRememberMe;
+
+class UserService
+{
+
+    const NOT_REMEMBER_SUFFIX = '_not_remember';
+
+    public $name;
+
+    protected $_notRememberMe;
+
+    protected $_auth;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+
+        $this->_auth = new AuthSession($name);
+
+        $this->_notRememberMe = new NotRememberMe($name . static::NOT_REMEMBER_SUFFIX);
+    }
+
+    public function setUserId($id, bool $rememberMe = true)
+    {
+        $this->_auth->setAuthId($id);
+
+        if ($rememberMe)
+        {
+            $this->_notRememberMe->deleteToken();
+        }
+        else
+        {
+            $token = $this->_notRememberMe->generateToken();
+
+            $this->_notRememberMe->setToken($token);
+        }
+    }
+
+    public function getUserId()
+    {
+        if (!$this->_notRememberMe->validateToken())
+        {
+            $this->logout();
+
+            return null;
+        }
+
+        $return = $this->_auth->getAuthId();
+
+        if (!$return)
+        {
+            $this->_notRememberMe->deleteToken();
+        }
+
+        return $return;
+    }
+
+    public function logout()
+    {
+        $this->_auth->deleteAuthId();
+
+        $this->_notRememberMe->deleteToken();
+    }
+
+}
